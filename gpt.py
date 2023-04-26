e30ec26295849e53dcc08e656fc9e2e965d43009
wanne88 <125958804+wanne88@users.noreply.github.com>
2023-04-26 22:36:34 +0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,4 +1,16 @@
+from flask import Flask, request, jsonify
 from nomic.gpt4all import GPT4All
+
+app = Flask(__name__)
 m = GPT4All()
 m.open()
-m.prompt('write me a story about a lonely computer')
+
+@app.route("/", methods=["POST"])
+def predict():
+    data = request.get_json(force=True)
+    question = data["question"]
+    answer = m.prompt(question)
+    return jsonify(answer=answer)
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
