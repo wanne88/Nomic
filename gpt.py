e4928e84e2a97dbdbbe12bb3c64a029a7f373709
wanne88 <125958804+wanne88@users.noreply.github.com>
2023-04-26 23:38:09 +0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,6 +1,22 @@
+from flask import Flask, render_template, request
 from transformers import GPT2Tokenizer, GPT2Model
+
+app = Flask(__name__)
+
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2Model.from_pretrained('gpt2')
-text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/answer', methods=['POST'])
+def get_answer():
+    question = request.form['question']
+    encoded_input = tokenizer(question, return_tensors='pt')
+    output = model(**encoded_input)
+    answer = tokenizer.decode(output['logits'][0], skip_special_tokens=True)
+    return render_template('index.html', question=question, answer=answer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
